63a3889d6f14fb25da2b17a5fd1d60308ac70eb2
mayur0907 <121195884+mayur0907@users.noreply.github.com>
2023-04-13 13:43:17 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+const routes = require("./routes.js");
+
+const server = http.createServer(routes);
+
+server.on("connection", (stream) =>
+  console.log("Someone connected to our server!")
+);
+server.listen(4000);
